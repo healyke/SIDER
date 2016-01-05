e1b02d9f7fd56d70d3dcaa6923e11e6efc8b8030
Andrew Jackson <a.jackson@tcd.ie>
2016-01-05 15:52:08 +0000

    started process of adding documentation using Roxygen2

--- R/setTefEst.R
+++ R/setTefEst.R
@@ -1,11 +1,35 @@
+#' Specify the observation to be imputed.
+#' 
+#' @param species a binomial species name as a character string.
+#' @param habitat a character string of either "marine" or "terrestrial"
+#' @param Class the class of species as a character string, either "mammalia" or
+#'   "aves"
+#' @param tissue the tissue type as a character string taking one of
+#'   \code{c("blood", "claws", "collagen", "ceather", "hair", "kidney", "liver",
+#'   "milk", "muscle")}
+#' @param diet_type the diet type as a character string taking one of
+#'   \code{c("carnivore", "herbivore", "omnivore",  "pellet")}
+#' @param iso_13C ? WE DONT ACTUALLY WANT THIS?
+#' @param iso_15N ? WE DONT ACTUALLY WANT THIS?
+#' @param phylogeny The full phylogenetic tree or set of trees as a
+#'   (multi-)phylo object.
+#'   
+#' @return A dataframe object of row length 1 comprising a checked species for
+#'   imputation.
+#'   
+#' @export
+
 
 
 
 setTefEst <- function(species = c(), 
                       habitat = c("marine", "terrestrial", NA), 
                       Class = c("mammalia", "aves", NA), 
-                      tissue = c("blood", "claws", "collagen", "ceather", "hair", "kidney", "liver", "milk", "muscle", NA), 
-                      diet_type = c("carnivore", "herbivore", "omnivore",  "pellet", NA), 
+                      tissue = c("blood", "claws", "collagen", "ceather", 
+                                 "hair", "kidney", "liver", "milk", 
+                                 "muscle", NA), 
+                      diet_type = c("carnivore", "herbivore", 
+                                    "omnivore",  "pellet", NA), 
                       iso_13C = c(NA), 
                       iso_15N = c(NA), 
                       phylogeny = c()){
@@ -29,12 +53,14 @@ if(is.null(Class) == T){warning("Data for Class missing")
 			
 ##check tissue data
 if(is.null(tissue) == T){warning("Data for tissue missing")
-	} else if(any( tissue == c("blood", "claws", "Collagen", "Feather", "hair", "kidney", "liver", "milk", "muscle")) != T){
+	} else if(any( tissue == c("blood", "claws", "Collagen", "Feather", "hair", 
+	                           "kidney", "liver", "milk", "muscle")) != T){
 			warning("tissue levels do not match dataset from Healy et al 2016")}
 			
 ##check diet_type data
 if(is.null(diet_type) == T){warning("Data for diet_type missing")
-	} else if(any( diet_type == c("carnivore", "herbivore", "omnivore",  "pellet")) != T){
+	} else if(any( diet_type == c("carnivore", "herbivore", 
+	                              "omnivore",  "pellet")) != T){
 			warning("diet_type levels do not match dataset from Healy et al 2016")}
 
 ##check iso data is there
@@ -51,7 +77,8 @@ if(is.null(phylogeny) == T){
 	phylo_test <- phylogeny[[1]]
 }		else if((class(phylogeny) == "phylo") == T){
 	phylo_test <- phylogeny
-	}	else{cat("phylo not a phlyo or multiPhylo object: species presence in phylogeny not checked")}	
+}	else{cat("phylo not a phlyo or multiPhylo object: species presence in 
+	           phylogeny not checked")}	
 	
 #check if the species is in the tree
 if(any(phylo_test $tip.label == species) == T){
@@ -62,7 +89,11 @@ if(any(phylo_test $tip.label == species) == T){
 
 
 ###put it in a data.frame with stringsAsFactors turned off.
-new.data <- data.frame(species = species, habitat = habitat, Class = Class, tissue = tissue, diet_type = diet_type, iso_13C = iso_13C, delta13C = NA, iso_15N = iso_15N, delta15N = NA, stringsAsFactors = F) 
+new.data <- data.frame(species = species, habitat = habitat, Class = Class, 
+                       tissue = tissue, diet_type = diet_type, 
+                       iso_13C = iso_13C, delta13C = NA, 
+                       iso_15N = iso_15N, delta15N = NA, 
+                       stringsAsFactors = F) 
 	
 
 return(new.data)
--- vingettes/test-tefMulClean.R
+++ vingettes/test-tefMulClean.R
@@ -4,7 +4,7 @@
 
 library(FestR)
 library(devtools)
-#install_github('TGuillerme/mulTree', ref='master')
+#devtools::install_github('TGuillerme/mulTree', ref='master')
 library(mulTree)
 
 
@@ -12,18 +12,21 @@ library(mulTree)
 mydata<-read.csv(file=system.file("extdata", "FestR_data.csv", package = "FestR"),
                  header=T, stringsAsFactors = F)
 
-# re-oprder it into species
+# re-order it into species
 mydata <- mydata[order(mydata$species),]
 
 # ----------------------------------------------------------------
 #read in the two trees
+# These two are the original as bundled with FestR
 mammal_trees <- read.tree(system.file("extdata", "3firstFritzTrees.tre", package = "FestR"))
 bird_trees   <- read.tree(system.file("extdata", "3firstJetzTrees.tre", package = "FestR"))
 
 # TG: I just created a subsample of the BIG trees (all sp) to make it faster
 #combine them together
-combined_trees<-rTreeBind(x = mammal_trees, y = bird_trees, sample = 2, root.age = 250)
+combined_trees <- rTreeBind(x = mammal_trees, y = bird_trees, sample = 2, root.age = 250)
 
+# Select the species and tissue type to be estimated using FestR. 
+# This function also checks that the species is in the tree.
 new_data_test <- setTefEst(species = "Meles_meles", 
                            habitat = "terrestrial", 
                            Class = "mammalia", 
@@ -34,10 +37,21 @@ new_data_test <- setTefEst(species = "Meles_meles",
                            phylogeny = combined_trees )
 
 
+# Matches the tree to the data, and prunes the tree of species for which we dont
+# have isotope or other data,  removes the unecessary isotope, and removes data 
+# for which we dont have phylogenetic data.
+# It returns a mulTree class object which is required by the imputation 
+# algorithm.
 tef_data_badger.c <- tefMulClean(new.data = new_data_test, data = mydata, species_col_name = "species", trees =  combined_trees, class = "mammalia", isotope = "carbon")
 
-formula.c <- delta13C ~ iso_13C + diet_type + habitat
+# define the model to be used for prediction
+formula.c <- delta13C ~ diet_type + habitat
 
+# Run the model that performs imputation as part of model fitting.
 Tef_est <- tefMcmcglmm(mulTree.data = tef_data_badger.c, formula = formula.c)
 
+# Plot the aggregated posteriors of the estimated TEF from all chains,
+# as a MCMC line trace (left panel) and as a posterior density kernel 
+# (right panel)
+plot(Tef_est$tef_global)
 
