07a85e95bb8ed83171dbeb31879b7399313d5971
Kevin Healy <healyke@tcd.ie>
2016-04-01 14:35:38 +0100

    updated the test file

--- vingettes/test-tefMulClean.R
+++ vingettes/test-tefMulClean.R
@@ -1,11 +1,12 @@
 
 # Test document. To be used as an example etc.
 
-##this is only required while Thomas updates his Multree code
-library(FestR)
+
 if(!require(devtools)) install.packages("devtools")
+install_github("healyke/Fester", ref = "master")
 install_github("TGuillerme/mulTree", ref = "master")
 library(mulTree)
+library(FestR)
 
 
 #read in the data
@@ -43,18 +44,34 @@ new_data_test <- setTefEst(species = "Nestor_notabilis",
 # for which we dont have phylogenetic data.
 # It returns a mulTree class object which is required by the imputation 
 # algorithm.
-tef_data_kea.c <- tefMulClean(new.data = new_data_test, data = mydata, species_col_name = "species", trees =  combined_trees, taxonomic.class = "aves", isotope = "carbon")
-tef_data_kea.n <- tefMulClean(new.data = new_data_test, data = mydata, species_col_name = "species", trees =  combined_trees, taxonomic.class = "aves", isotope = "nitrogen")
+tef_data_kea.c <- tefMulClean(new.data = new_data_test, data = mydata, species_col_name = "species", 
+							  trees =  combined_trees, taxonomic.class = "aves", isotope = "carbon")
+tef_data_kea.n <- tefMulClean(new.data = new_data_test, data = mydata, species_col_name = "species", 
+							  trees =  combined_trees, taxonomic.class = "aves", isotope = "nitrogen")
 
 # define the model to be used for prediction
 formula.c <- delta13C ~ source.iso.13C + diet.type + habitat
+
 formula.n <- delta15N ~ source.iso.15N + diet.type + habitat
+
 random.terms = ~ animal + sp.col + tissue
 
-Tef_est.c <- tefMcmcglmm(mulTree.data = tef_data_kea.c, formula = formula.c, random.terms = random.terms)
-Tef_est.n <- tefMcmcglmm(mulTree.data = tef_data_kea.n, formula = formula.n, random.terms = random.terms)
+prior_tef <- list(R = list(V = 1/4, nu=0.002), G = list(G1=list(V = 1/4, nu=0.002),
+					G2=list(V = 1/4, nu=0.002), G3=list(V = 1/4, nu=0.002)))
 
+
+###set directory somewhere that you can access the model files run by the model
+#setwd("c:/path/to/my/directory/at/work")
+
+Tef_est.c <- tefMcmcglmm(mulTree.data = tef_data_kea.c, formula = formula.c, random.terms = random.terms, 
+						 prior = prior_tef, output = "kea_c_test", nitt = c(1200000),  thin = c(500),  
+						 burnin = c(200000), no.chains = c(2), convergence =  c(1.1), ESS = c(1000))
+Tef_est.n <- tefMcmcglmm(mulTree.data = tef_data_kea.n, formula = formula.n, random.terms = random.terms,
+						 prior = prior_tef, output = "kea_n_test", nitt = c(1200000),  thin = c(500),  
+						 burnin = c(200000), no.chains = c(2), convergence =  c(1.1), ESS = c(1000))
+hist(Tef_est.c$tef_global)
 median(Tef_est.c$tef_global)
+hist(Tef_est.n$tef_global)
 median(Tef_est.n$tef_global)
 
 # Plot the aggregated posteriors of the estimated TEF from all chains,
