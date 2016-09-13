d06397728a50ec295b60d654badb46442918930b
Andrew Jackson <a.jackson@tcd.ie>
2016-09-13 15:10:16 -0700

    fixed "note" on R CMD check
    still have to sort out the warning re compression of data via
    —resave-data option that i can’t seem to get to work

--- R/refresh.isotope_data.R
+++ R/refresh.isotope_data.R
@@ -1,10 +1,17 @@
-#Internal utility function for refreshing the istotop_data dataset (not exported)
-#This function rapidly updates the isotope_data.rda file once one updates the TESIR_data.csv file.
+#Internal utility function for refreshing the istotop_data dataset (not
+#exported) This function rapidly updates the isotope_data.rda file once one
+#updates the TESIR_data.csv file.
 refresh.isotope_data <- function(data = "TESIR_data.csv") {
     # Read the csv file from the package data
-    isotope_data <- read.csv(file = system.file("extdata", data, package = "TESIR"), header = TRUE, stringsAsFactors = FALSE)
+    isotope_data <- utils::read.csv(file = system.file("extdata", data, 
+                                                       package = "TESIR"), 
+                                    header = TRUE, 
+                                    stringsAsFactors = FALSE)
+    
     # Save it as the isotope_data set in the manual
     save(isotope_data, file = "../data/isotope_data.rda")
+    
     # Make sure to update the manual
-    cat("isotope_data.rda file successfully updated.\nDon't forget to update the man page in \\man\\TESIR.R if necessary.")
+    cat("isotope_data.rda file successfully updated.\n
+        Don't forget to update the man page in \\man\\TESIR.R if necessary.")
 }
