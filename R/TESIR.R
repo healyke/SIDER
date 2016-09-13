6ac9c77aae1b69e3837589ea9585f07d840b0a61
Thomas Guillerme <guillert@tcd.ie>
2016-09-13 16:00:11 +0100

    add isotope_data as part of the package.

--- R/TESIR.R
+++ R/TESIR.R
@@ -19,3 +19,46 @@
 #' @name TESIR
 NULL
 #> NULL
+
+
+#' Example Aves and Mammalia lifespan for the mulTree package
+#'
+#' This is a dataset containing lifespan data from 192 species of birds and mammals.
+#'
+#' @name isotope_data
+#' @docType data
+#'
+#' @format a \code{data.frame} containing isotopic data derived from controlled feeding trials on birds and mammals.
+#' \describe{
+#'        \item{species}{The species binomial name.}
+#'        \item{habitat}{The species habitat (either \code{marine} or \code{terrestrial}).}
+#'        \item{taxonomic.class}{The species taxonomic class (either \code{aves} or \code{mammalia}).}
+#'        \item{tissue}{The measured tissue (\code{liver}, \code{blood}, \code{kidney}, \code{muscle}, \code{hair}, \code{milk}, \code{feather}, \code{claws} or , \code{Collagen}.}
+#'        \item{source.iso.13C}{The isotopic 13C delta of the food source.}
+#'        \item{source.iso.15N}{The isotopic 15N delta of the food source.}
+#'        \item{delta13C}{The tropic discrimination factor for delta 13C.}
+#'        \item{delta15N}{The tropic discrimination factor for delta 15N}
+#' }
+#' @references [ADD REFERENCES]
+#'
+#' @keywords datasets
+NULL
+
+# #' Example dataset for the \code{mulTree} package
+# #'
+# #' This is a dataset containing lifespan data and trees from Healy et al (2014)
+# #'
+# #' @name lifespan
+# #' @docType data
+# #'
+# #' @format Contains a \code{data.frame} and two \code{multiPhylo} objects:
+# #'    \describe{
+# #'        \item{lifespan_volant}{A \code{data.frame} object of five variables for 192 species (see \code{\link{lifespan_volant_192taxa}}).}
+# #'        \item{trees_aves}{A \code{multiPhylo} object of two trees of 58 bird species. The tip names are the binomial names of the species.}
+# #'        \item{trees_mammalia}{A a \code{multiPhylo} object of two trees of 134 mammal species. The tip names are the binomial names of the species.}
+# #'    }
+# #'
+# #' @references Healy, K., Guillerme, T., Finlay, S., Kane, A., Kelly, S, B, A., McClean, D., Kelly, D, J., Donohue, I., Jackson, A, L., Cooper, N. (2014) Ecology and mode-of-life explain lifespan variation in birds and mammals. Proceedings of the Royal Society B 281, 20140298c
+# #'
+# #' @keywords datasets
+# NULL
