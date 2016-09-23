e7da2bc1606d777cdd3a735f5413f5855cfd3ae1
Andrew Jackson <a.jackson@tcd.ie>
2016-09-23 11:30:27 -0700

    added xz compression option to save to *.rda

--- R/refresh.isotope_data.R
+++ R/refresh.isotope_data.R
@@ -1,17 +1,29 @@
-#Internal utility function for refreshing the istotop_data dataset (not
-#exported) This function rapidly updates the isotope_data.rda file once one
-#updates the TESIR_data.csv file.
-refresh.isotope_data <- function(data = "TESIR_data.csv") {
+#' @title Refreshes the isotope dataset
+#'   
+#' @description Internal utility function for refreshing the isotope_data 
+#'   dataset (not exported) This function rapidly updates the isotope_data.rda 
+#'   file once one updates the TESIR_data.csv file.
+#'   
+#' @param data which is typically the SIDER_data.csv file containing the dataset
+#'   used for model fitting and imputation.
+#'   
+#' @return Saves the updated file to ../data/isotope_data.rda for use within the
+#'   pacakge
+#'   
+#' @author Thomas Guillerme
+
+
+refresh.isotope_data <- function(data = "SIDER_data.csv") {
     # Read the csv file from the package data
     isotope_data <- utils::read.csv(file = system.file("extdata", data, 
-                                                       package = "TESIR"), 
+                                                       package = "SIDER"), 
                                     header = TRUE, 
                                     stringsAsFactors = FALSE)
     
     # Save it as the isotope_data set in the manual
-    save(isotope_data, file = "../data/isotope_data.rda")
+    save(isotope_data, file = "../data/isotope_data.rda", compress = 'xz')
     
     # Make sure to update the manual
     cat("isotope_data.rda file successfully updated.\n
-        Don't forget to update the man page in \\man\\TESIR.R if necessary.")
+        Don't forget to update the man page in \\man\\SIDER.R if necessary.")
 }
