84b6cffced8330c1aaafa19d3a3f399af28e3af6
Thomas Guillerme <guillert@tcd.ie>
2016-09-26 17:11:55 +0100

    Added indents and removed useless == TRUE
    in an if statement, when the output of the test is a logical, no need
    to do if(test == TRUE). In this case it doubles execution time: it runs
    the test, outputs a logical, and check if this logical is TRUE and if
    all that is TRUE, does the if. It can be simplified to just if(test)
    where it asses simply if(TRUE) and then does the if. Same for the
    !=TRUE. You can replace them by if(!test).

--- R/setTdfEst.R
+++ R/setTdfEst.R
@@ -40,105 +40,90 @@ setTdfEst <- function(species = c(),
     
 
 
-###check if each of the inputs matches the categories we want.
+  ###check if each of the inputs matches the categories we want.
 
-###check species
-if(is.null(species) == TRUE){stop("species missing")}
+  ###check species
+  if(is.null(species)){
+    stop("species missing")
+  }
 
-##check habitat data
-if(is.null(habitat) == TRUE){
-  #TG: not sure if we want a warning here! Maybe a stop?
-    warning("Data for habitat missing") 
-} else if(any( habitat == c("marine", "terrestrial")) != TRUE){
-  #TG: not sure if we want a warning here! Maybe a stop?
-    warning("habitat levels do not match dataset from Healy et al 2016") 
-}
+  ##check habitat data
+  if(is.null(habitat)){
+      warning("Data for habitat missing") 
+  } else if(!any( habitat == c("marine", "terrestrial"))){
+      warning("habitat levels do not match dataset from Healy et al 2016") 
+  }
 
-##check class data
-if(is.null(taxonomic.class) == TRUE){
-    warning("Data for taxonomic.class missing")
-} else if(any(taxonomic.class == c("mammalia", "aves")) != TRUE){
-    warning("taxonomic.class levels do not match dataset from 
-            Healy et al 2016")
-}
-            
-##check tissue data
-if(is.null(tissue) == TRUE){
-    warning("Data for tissue missing")
-} else if(any( tissue == c("blood", "claws", "collagen", "feather", 
-                           "hair", "kidney", "liver", "milk", 
-                           "muscle")) != TRUE){
-    warning("tissue levels do not match dataset from Healy et al 2016")
-}
-            
-##check diet_type data
-if(is.null(diet.type) == TRUE){
-    warning("Data for diet.type missing")
-} else if(any( diet.type == c("carnivore", 
-                              "herbivore", 
-                              "omnivore",  
-                              "pellet")) != TRUE) {
-    warning("diet.type levels do not match dataset from Healy et al 2016")
-}
+  ##check class data
+  if(is.null(taxonomic.class)){
+      warning("Data for taxonomic.class missing")
+  } else if(!any(taxonomic.class == c("mammalia", "aves"))){
+      warning("taxonomic.class levels do not match dataset from 
+              Healy et al 2016")
+  }
+
+  ##check tissue data
+  if(is.null(tissue)){
+      warning("Data for tissue missing")
+  } else if(!any( tissue == c("blood", "claws", "collagen", "feather", 
+                             "hair", "kidney", "liver", "milk", 
+                             "muscle"))){
+      warning("tissue levels do not match dataset from Healy et al 2016")
+  }
+
+  ##check diet_type data
+  if(is.null(diet.type)){
+      warning("Data for diet.type missing")
+  } else if(!any( diet.type == c("carnivore", 
+                                "herbivore", 
+                                "omnivore",  
+                                "pellet"))) {
+      warning("diet.type levels do not match dataset from Healy et al 2016")
+  }
 
 
-##check iso data is there and warn user if they are going to use it
-  if((is.null(source.iso.13C)|is.null(source.iso.15N)) == TRUE){
+  ##check iso data is there and warn user if they are going to use it
+  if(is.null(source.iso.13C) | is.null(source.iso.15N)){
     source.iso.13C <- NA
     source.iso.15N <- NA
-  } else if(any(c(class(source.iso.13C), class(source.iso.15N)) == "numeric") 
-            != TRUE){
-    warning("source isotopic data not numeric")
+  } else if(any(c(class(source.iso.13C), class(source.iso.15N)) != "numeric")){
+    warning("Source isotopic data not numeric")
     warning("Only include isotopic food isotopic values if derived from controlled dietary settings")
-        } else {
-    warning("Only include isotopic food isotopic values if derived from controlled dietary settings")
+  } else {
+    #TG: is this warning necessary?
+    #warning("Only include isotopic food isotopic values if derived from controlled dietary settings")
   } 
    
-###check if there is a tree and what type of tree it is
-if(is.null(tree) == TRUE){     
-    cat("phylogeny is missing: species presence in phylogeny not checked")
-} else if((class(tree) == "multiPhylo") == TRUE){
-    phylo_test <- tree[[1]]
-} else if((class(tree) == "phylo") == TRUE){
-    phylo_test <- tree
-} else{
-    cat("phylogeny is not a phylo or multiPhylo object: species presence 
-        in phylogeny not checked")
+  ###check if there is a tree and what type of tree it is
+  if(is.null(tree)){
+      cat("phylogeny is missing: species presence in phylogeny not checked")
+  } else if(class(tree) == "multiPhylo"){
+      phylo_test <- tree[[1]] 
+  } else if(class(tree) == "phylo"){
+      phylo_test <- tree
+  } else{
+      cat("phylogeny is not a phylo or multiPhylo object: species presence 
+          in phylogeny not checked")
+  }
+      
+  #check if the species is in the tree and report finding.
+  if(any(phylo_test $tip.label == species)){
+      cat(species, "present in phylogeny")
+  } else {
+      cat(species, "not present in phylogeny")
+  }
+
+  ###put it in a data.frame with stringsAsFactors turned off.
+  new.data <- data.frame(species = species, 
+                         habitat = habitat, 
+                         taxonomic.class = taxonomic.class, 
+                         tissue = tissue, 
+                         diet.type = diet.type, 
+                         source.iso.13C = source.iso.13C, 
+                         delta13C = NA, 
+                         source.iso.15N = source.iso.15N, 
+                         delta15N = NA, 
+                         stringsAsFactors = F) 
+
+  return(new.data)
 }
-    
-#check if the species is in the tree and report finding.
-if(any(phylo_test $tip.label == species) == TRUE){
-    cat(species, "present in phylogeny")
-} else {
-    cat(species, "not present in phylogeny")
-}
-
-###put it in a data.frame with stringsAsFactors turned off.
-new.data <- data.frame(species = species, 
-                       habitat = habitat, 
-                       taxonomic.class = taxonomic.class, 
-                       tissue = tissue, 
-                       diet.type = diet.type, 
-                       source.iso.13C = source.iso.13C, 
-                       delta13C = NA, 
-                       source.iso.15N = source.iso.15N, 
-                       delta15N = NA, 
-                       stringsAsFactors = F) 
-
-return(new.data)
-}
-
-
-
-
-    
-
-
-
-
-
-
-
-
-    
-    
